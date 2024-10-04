5c4fe359495525fcd469b12f4b7a22cf57131713
Anya Aven <leafeon96@gmail.com>
2024-10-04 18:53:29 -0400

    arrayContains

--- recursion/array-contains/arrayContains.ts
+++ recursion/array-contains/arrayContains.ts
@@ -1,9 +1,18 @@
 type tRecurNumArray = (number | tRecurNumArray)[];
 
 /** Does array contain item? (recursive search) */
-
+// 3 minutes
 function arrayContains(nums: tRecurNumArray, sought: number): boolean {
-  return false;
+  for (const val of nums) {
+    if (val === sought) return true
+
+    if (Array.isArray(val)) return arrayContains(val, sought)
+  }
+
+  return false
 }
+
+// Could solve with binary search
+
 
 export { arrayContains };
